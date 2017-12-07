0f815c8062fa03249a419e5bc85ee135dd0ed8c5
Andrey Luiz <andreyluiz.developer@gmail.com>
2017-12-06 23:18:44 -0200

    Create the advertisements grid.

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import Advertisement from './components/Advertisement';
 import styles from './styles.css';
 
 const App = () => (
-  <h2 className={styles.title}>Hello!</h2>
+  <div className={styles.content}>
+    <div className={styles.row}>
+      <div className={styles.column}>
+        <Advertisement />
+      </div>
+      <div className={styles.column}>
+        <Advertisement />
+      </div>
+      <div className={styles.column}>
+        <Advertisement />
+      </div>
+    </div>
+    <div className={styles.row}>
+      <div className={styles.column}>
+        <Advertisement />
+      </div>
+      <div className={styles.column}>
+        <Advertisement />
+      </div>
+      <div className={styles.column}>
+        <Advertisement />
+      </div>
+    </div>
+    <div className={styles.row}>
+      <div className={styles.column}>
+        <Advertisement />
+      </div>
+    </div>
+  </div>
 );
 
 export default App;
